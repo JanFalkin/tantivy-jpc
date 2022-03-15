e0576667fac1681e288eabd1c934b7619e6a2ac4
janfalkin <jan.falkin@eluv.io>
2022-03-14 19:04:56 -0700

    first attempt automated build and branch

--- .github/run_go.sh
+++ .github/run_go.sh
@@ -2,3 +2,8 @@
 
 working_dir=$PWD
 echo $working_dir
+pushd go-client
+go test ./...
+popd
+cp -u target/release/libtantivy_jpc.so go-client/tantivy/packaged/lib/linux-amd64/
+cp -u target/tantivy_jpc.h go-client/tantivy/packaged/include/
