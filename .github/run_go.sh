e5322509bb5c36c5d6764f3de95a00d69062c9f6
janfalkin <jan.falkin@eluv.io>
2022-05-13 13:52:52 -0700

    copy header too but just once, remove ununsed script

--- .github/run_go.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-working_dir=$PWD
-echo $working_dir
-pushd go-client
-go test ./...
-popd
-cp -u target/release/libtantivy_jpc.so go-client/tantivy/packaged/lib/linux-amd64/
-cp -u target/tantivy_jpc.h go-client/tantivy/packaged/include/
