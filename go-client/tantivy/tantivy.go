717574359ca160a1c0b3823a8cb8293ff9c92543
janfalkin <jan.falkin@eluv.io>
2023-02-26 10:34:48 -0800

    better optional behavior

--- go-client/tantivy/tantivy.go
+++ go-client/tantivy/tantivy.go
@@ -57,16 +57,16 @@ type TSearcher struct {
 
 func (s *TSearcher) Search(topLimit ...uint64) (string, error) {
 	args := msi{}
-	if len(topLimit) == 1 {
-		args["top_limit"] = topLimit
+	if len(topLimit) >= 1 {
+		args["top_limit"] = topLimit[0]
 	}
 	return s.callTantivy("searcher", "search", args)
 }
 
 func (s *TSearcher) FuzzySearch(topLimit ...uint64) (string, error) {
 	args := msi{}
-	if len(topLimit) == 1 {
-		args["top_limit"] = topLimit
+	if len(topLimit) >= 1 {
+		args["top_limit"] = topLimit[0]
 	}
 	return s.callTantivy("fuzzy_searcher", "fuzzy_searcher", msi{})
 }
