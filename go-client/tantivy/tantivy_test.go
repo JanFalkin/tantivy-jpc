807520d761b23217a6aaea4368645bb7969c1682
Jan Falkin <jan.falkin@eluv.io>
2023-08-07 15:29:48 -0700

    docset snippet test

--- go-client/tantivy/tantivy_test.go
+++ go-client/tantivy/tantivy_test.go
@@ -460,13 +460,82 @@ func TestDocsetSearch(t *testing.T) {
 
 	require.EqualValues(t, "The Old Man and the Sea", results["doc"].(jm)["title"].([]interface{})[0].(string))
 
-	// searcherAgain, err := qp.ParseQuery("order:2")
-	// require.NoError(t, err)
-	// s, err = searcherAgain.SearchRaw()
-	// require.NoError(t, err)
-	// err = json.Unmarshal([]byte(s), &results)
-	// require.NoError(t, err)
-	// require.EqualValues(t, "Of Mice and Men", results[0]["title"].([]interface{})[0].(string))
+}
+
+func TestDocsetSnippetSearch(t *testing.T) {
+	builder, err := NewBuilder("")
+	require.NoError(t, err)
+	idxFieldTitle, err := builder.AddTextField("title", TEXT, true, true, "")
+	require.NoError(t, err)
+	require.EqualValues(t, 0, idxFieldTitle)
+	idxFieldBody, err := builder.AddTextField("body", TEXT, true, true, "")
+	require.NoError(t, err)
+	require.EqualValues(t, 1, idxFieldBody)
+	doc, err := builder.Build()
+	require.NoError(t, err)
+	doc1, err := doc.Create()
+	require.NoError(t, err)
+	require.EqualValues(t, 1, doc1)
+	doc2, err := doc.Create()
+	require.NoError(t, err)
+	require.EqualValues(t, 2, doc2)
+	_, err = doc.AddText(idxFieldTitle, "The Old Man and the Sea", doc1)
+	require.NoError(t, err)
+	_, err = doc.AddText(idxFieldBody, "He was an old man who fished alone in a skiff in the Gulf Stream and he had gone eighty-four days now without taking a fish. The water was warm but fishless.", doc1)
+	require.NoError(t, err)
+	_, err = doc.AddText(idxFieldTitle, "Of Mice and Men", doc2)
+	require.NoError(t, err)
+	_, err = doc.AddText(idxFieldBody, `A few miles south of Soledad, the Salinas River drops in close to the hillside
+	bank and runs deep and green. The water is warm too, for it has slipped twinkling
+	over the yellow sands in the sunlight before reaching the narrow pool. On one
+	side of the river the golden foothill slopes curve up to the strong and rocky
+	Gabilan Mountains, but on the valley side the water is lined with trees—willows
+	fresh and green with every spring, carrying in their lower leaf junctures the
+	debris of the winter's flooding; and sycamores with mottled, white, recumbent
+	limbs and branches that arch over the pool`, doc2)
+	require.NoError(t, err)
+
+	indexer, err := doc.CreateIndex()
+	require.NoError(t, err)
+
+	idw, err := indexer.CreateIndexWriter()
+	require.NoError(t, err)
+	opst1, err := idw.AddDocument(doc1)
+	require.NoError(t, err)
+	require.EqualValues(t, 0, opst1)
+	opst2, err := idw.AddDocument(doc2)
+	require.NoError(t, err)
+	require.EqualValues(t, 1, opst2)
+	fmt.Printf("op1 = %v op2 = %v\n", opst1, opst2)
+	idCommit, err := idw.Commit()
+	require.NoError(t, err)
+	fmt.Printf("commit id = %v", idCommit)
+
+	rb, err := indexer.ReaderBuilder()
+	require.NoError(t, err)
+	qp, err := rb.Searcher()
+	require.NoError(t, err)
+
+	_, err = qp.ForIndex([]string{"title", "body"})
+	require.NoError(t, err)
+
+	searcher, err := qp.ParseQuery("narrow pool")
+	require.NoError(t, err)
+	s, err := searcher.Docset(true, 20, 0)
+	require.NoError(t, err)
+	results := map[string]interface{}{}
+	err = json.Unmarshal([]byte(s), &results)
+	require.NoError(t, err)
+	resElement, ok := results["docset"].([]interface{})[0].(jm)
+	require.EqualValues(t, true, ok)
+	sDoc, err := searcher.GetDocument(true, float32(resElement["score"].(float64)), uint32(resElement["doc_id"].(float64)), uint32(resElement["segment_ord"].(float64)), 1)
+	require.NoError(t, err)
+	log.Info(sDoc)
+	err = json.Unmarshal([]byte(sDoc), &results)
+	require.NoError(t, err)
+	resDoc := results["doc"].(jm)
+	require.EqualValues(t, "Of Mice and Men", resDoc["title"].([]interface{})[0].(string))
+	require.NotEmpty(t, results["snippet_html"])
 }
 
 func TestStops(t *testing.T) {
